9ca4e77f54a91760d7e6087c9d1f94761faed89b
aalarie134 <91813705+aalarie134@users.noreply.github.com>
2021-12-16 11:54:28 -0500

    fisrt try part 2

--- src/day3.java
+++ src/day3.java
@@ -6,32 +6,63 @@ public class day3 {
     public static void main(String args[]) throws FileNotFoundException {
 
         File file = new File("input.txt");
-        Scanner scan = new Scanner(file);
         counter[] count = new counter[12];
 
         for(int j = 0 ; j < 12 ; j++){
             count[j] = new counter();
         }
-        while (scan.hasNext()) {
-            char[] line = scan.next().toCharArray();
-            System.out.println(Arrays.toString(line));
-            for (int k = 0; k < 12; k++) {
-                    count[k].add(line[k]);
+//        while (scan.hasNext()) {
+//            char[] line = scan.next().toCharArray();
+//            System.out.println(Arrays.toString(line));
+//            for (int k = 0; k < 12; k++) {
+//                    count[k].add(line[k]);
+//            }
+//        }
+//        int[] answer = new int[12];
+//        for( int i = 0 ; i < 12 ; i++){
+//            if(count[i].getNumber0() > count[i].getNumber1()){
+//                answer[i] = 0;
+//            }
+//            else{
+//                answer[i] = 1;
+//            }
+//        }
+
+//        System.out.println(Arrays.toString(answer));
+//        double end = converter.convert(answer) * converter.convert(converter.flip(answer));
+//        System.out.println(end);
+
+        // PART 2
+        int number = 0;
+        int iteration = 1;
+        int[] memoryMax= new int[12];
+        int[] memoryMin =  new int[12];
+        while( number != 1) {
+            Scanner scan = new Scanner(file);
+            while (scan.hasNext()) {
+                number = 0;
+                char[] line = scan.next().toCharArray();
+//                System.out.println(Arrays.toString(line));
+                for (int k = 0; k <= iteration; k++) {
+                    if(iteration == 1 || line[k] == memoryMax[k]) {
+                        count[k].add(line[k]);
+
+                        if( number == 0) {
+                            char[] tempMax = line;
+                        }
+                        number++;
+                    }
+                }
+                if(count[iteration].getNumber0() > count[iteration].getNumber1()){
+                    memoryMax[iteration] = 0;
+                }
+                else{
+                    memoryMax[iteration] = 1;
+                }
             }
-        }
-        int[] answer = new int[12];
-        for( int i = 0 ; i < 12 ; i++){
-            if(count[i].getNumber0() > count[i].getNumber1()){
-                answer[i] = 0;
-            }
-            else{
-                answer[i] = 1;
-            }
+            iteration++;
         }
 
-        System.out.println(Arrays.toString(answer));
-        double end = converter.convert(answer) * converter.convert(converter.flip(answer));
-        System.out.println(end);
 
     }
 }
